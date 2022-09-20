d5fab7f67bd5327314602226e6be0e981cde8e5f
bjelanovicsanja <bjelanovicsanja98@gmail.com>
2022-09-20 22:21:01 +0200

    print calculation

---
+++ src/printCalculation.java
@@ -0,0 +1,11 @@
+public class printCalculation {
+    public static void main(String[] args) {
+        int a = 299;
+        int b = 800;
+        int suma = a + b;
+        System.out.println("       " + a);
+        System.out.println("+      " + b);
+        System.out.println("----------");
+        System.out.println("      " + suma);
+    }
+}

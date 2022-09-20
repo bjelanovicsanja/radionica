4421b5c35645fd3e2a8fb657a45a042c3e29fff8
bjelanovicsanja <bjelanovicsanja98@gmail.com>
2022-09-20 22:36:59 +0200

    escaping

---
+++ src/Escaping.java
@@ -0,0 +1,9 @@
+public class Escaping {
+    public static void main(String[] args) {
+        System.out.println("Name\t" + "Height\t" + "Gender");
+        System.out.println("----------------------");
+        System.out.println("Anna\t" + "5\'4\"\t" + "F");
+        System.out.println("Test\t" + "6\'2\"\t" + "M");
+        System.out.println("Ben\t\t"  + "5\'7\"\t" + "M");
+    }
+}

48fdf2f5bda33e4cb49903659810bca03c316904
bjelanovicsanja <bjelanovicsanja98@gmail.com>
2022-09-22 13:38:34 +0200

    Ćbugfix suma->sum

--- src/printCalculation.java
+++ src/printCalculation.java
@@ -2,10 +2,10 @@ public class printCalculation {
     public static void main(String[] args) {
         int a = 299;
         int b = 800;
-        int suma = a + b;
+        int sum = a + b;
         System.out.println("       " + a);
         System.out.println("+      " + b);
         System.out.println("----------");
-        System.out.println("      " + suma);
+        System.out.println("      " + sum);
     }
 }

9757511599bc58c6949d06f2bdbd3f61e248c8f6
bjelanovicsanja <bjelanovicsanja98@gmail.com>
2022-09-23 08:27:49 +0200

    bugfix message characters

--- src/MessageCharacters.java
+++ src/MessageCharacters.java
@@ -4,12 +4,15 @@ public class MessageCharacters {
         System.out.println("Enter a message : ");
         Scanner scan = new Scanner(System.in);
         String message;
-        message=scan.nextLine();
-        if(message.length() >= 6 && message.length() <= 10){
-            System.out.println("Your message is between 6 and 10. Well done!");
+        message = scan.nextLine();
+        if (message.length() >= 6) {
+            if (message.length() <= 10) {
+                System.out.println("Message is: " + message);
+            } else {
+                System.out.println("Your message have more than 10 characters. It has to be between 6 - 10 characters.");
+            }
         } else {
-            System.out.println("Your message is not between 6 and 10. Try again!");
+            System.out.println("Your message have less than 6 characters. It has to be between 6-10 characters.");
         }
-        System.out.println("Message is: " + message);
     }
 }

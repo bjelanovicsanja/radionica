8958feda9de77132450c00384bdf2c970763274a
bjelanovicsanja <bjelanovicsanja98@gmail.com>
2022-09-23 08:14:07 +0200

    bugfix printStars

--- src/PrintStars.java
+++ src/PrintStars.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 public class PrintStars {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         System.out.println("Enter a number from 1 to 6: ");
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        for(int i=0;i<n;i++)
-        {
-            System.out.print("* ");
-        }
-            System.out.println();
-        }
+        String message = switch (n) {
+            case 1 -> "*";
+            case 2 -> "**";
+            case 3 -> "***";
+            case 4 -> "****";
+            case 5 -> "*****";
+            case 6 -> "*******";
+            default -> "invalid number of ";
+        };
+        System.out.println("You enter " + message + " stars");
     }
+}

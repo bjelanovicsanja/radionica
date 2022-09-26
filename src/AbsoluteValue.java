144763818f7878e2fb256f26a4ccf016f9089dde
bjelanovicsanja <98027767+bjelanovicsanja@users.noreply.github.com>
2022-09-26 11:43:30 +0200

    Update AbsoluteValue.java

--- src/AbsoluteValue.java
+++ src/AbsoluteValue.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Enter number: ");
+        System.out.println("Test conflict");
         double number = input.nextDouble();
         System.out.println("Number is " + number + " and abs of that number is " + Math.abs(number));
     }

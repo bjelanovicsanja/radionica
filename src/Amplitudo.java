4b93c40f6a2876a326a9b99150ba6942f219f29a
bjelanovicsanja <bjelanovicsanja98@gmail.com>
2022-09-29 13:37:41 +0200

    Constructor exercise

---
+++ src/Amplitudo.java
@@ -0,0 +1,11 @@
+public class Amplitudo {
+    private int salary = 200;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
